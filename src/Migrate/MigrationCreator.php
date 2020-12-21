5e5d712fdc65bb18f996b9f8fcc7cdd727ab46ba
titrxw <1120309488@qq.com>
2020-12-21 22:47:34 +0800

    修改database 6.0 7.0兼容问题

--- src/Migrate/MigrationCreator.php
+++ src/Migrate/MigrationCreator.php
@@ -13,9 +13,34 @@
 namespace W7\DatabaseTool\Migrate;
 
 use Illuminate\Database\Migrations\MigrationCreator as MigrationCreatorAbstract;
+use Illuminate\Filesystem\Filesystem;
 use Illuminate\Support\Str;
 
 class MigrationCreator extends MigrationCreatorAbstract {
+	protected $customStubPath = '';
+
+	public function __construct(Filesystem $files) {
+		parent::__construct($files, '');
+	}
+
+	protected function getStub($table, $create) {
+		if (is_null($table)) {
+			$stub = $this->files->exists($customPath = $this->customStubPath.'/blank.stub')
+				? $customPath
+				: $this->stubPath().'/blank.stub';
+		} elseif ($create) {
+			$stub = $this->files->exists($customPath = $this->customStubPath.'/create.stub')
+				? $customPath
+				: $this->stubPath().'/create.stub';
+		} else {
+			$stub = $this->files->exists($customPath = $this->customStubPath.'/update.stub')
+				? $customPath
+				: $this->stubPath().'/update.stub';
+		}
+
+		return $this->files->get($stub);
+	}
+
 	/**
 	 * Get the path to the stubs.
 	 *
